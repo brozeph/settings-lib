e1b9d43d566c8e86b0ea290943859b974e7fbded
Joshua Thomas <joshua.thomas@gmail.com>
2013-11-25 13:28:03 -0800

    fleshing out unit tests for environment and command line config loading

--- test/lib/settings.js
+++ test/lib/settings.js
@@ -2,15 +2,12 @@ describe('settings', function () {
 
 	var
 		defaultOptions,
-		settingsLib;
+		settingsLib = requireWithCoverage('settings');
 
 	beforeEach(function () {
 		defaultOptions = {
 			baseConfigPath : 'test/baseConfig.json'
 		};
-
-		settingsLib = null;
-		settingsLib = requireWithCoverage('settings');
 	});
 
 	describe('#initialize', function () {
@@ -53,7 +50,7 @@ describe('settings', function () {
 			});
 		});
 
-		it('should not load branch config if not present', function (done) {
+		it('should not load environment config if environment config file not found', function (done) {
 			process.env.NODE_ENV = 'test';
 
 			settingsLib.initialize(
@@ -69,7 +66,7 @@ describe('settings', function () {
 				});
 		});
 
-		it('should load branch config if present', function (done) {
+		it('should load environment config if present', function (done) {
 			process.env.NODE_ENV = 'test';
 			defaultOptions.environmentSearchPaths = ['./test'];
 
@@ -86,7 +83,24 @@ describe('settings', function () {
 				});
 		});
 
-		it('should not allow extra fields to propegate from config sources if baseConfig is set', function (done) {
+		it('should choke loading badly formatted environment config file', function (done) {
+			process.env.NODE_ENV = 'bad';
+			defaultOptions.environmentSearchPaths = ['./test'];
+
+			settingsLib.initialize(
+				defaultOptions,
+				function (err, settings) {
+					should.exist(err);
+					should.not.exist(settings);
+
+					// clean up
+					delete process.env.NODE_ENV;
+
+					done();
+				});
+		});
+
+		it('should not allow extra fields to propegate from environment config if baseConfig is set', function (done) {
 			process.env.NODE_ENV = 'test';
 			defaultOptions.environmentSearchPaths = ['./test'];
 
@@ -104,7 +118,25 @@ describe('settings', function () {
 				});
 		});
 
-		it('should propegate all fields from config sources if baseConfig is not set', function (done) {
+		it('should override base config with environment config', function (done) {
+			process.env.NODE_ENV = 'test';
+			defaultOptions.environmentSearchPaths = ['./test'];
+
+			settingsLib.initialize(
+				defaultOptions,
+				function (err, settings) {
+					should.not.exist(err);
+					should.exist(settings);
+					should.exist(settingsLib.environmentConfig);
+					settings["test-key"].should.equal('test-value-override');
+
+					delete process.env.NODE_ENV;
+
+					done();
+				});
+		});
+
+		it('should propegate all fields from environment config if baseConfig is not set', function (done) {
 			process.env.NODE_ENV = 'test';
 			delete defaultOptions.baseConfigPath;
 			defaultOptions.environmentSearchPaths = ['./test'];
@@ -118,6 +150,152 @@ describe('settings', function () {
 					should.exist(settings["extra-key"]);
 
 					delete process.env.NODE_ENV;
+
+					done();
+				});
+		});
+
+		it('should not set environment config on library when no environment is specified', function (done) {
+			settingsLib.initialize(
+				defaultOptions,
+				function (err, settings) {
+					should.not.exist(err);
+					should.exist(settings);
+					should.not.exist(settingsLib.environmentConfig);
+					settings["test-key"].should.equal('test-value');
+
+					done();
+				});
+		});
+
+		it('should load command line config if present', function (done) {
+			process.argv.push('--config-file');
+			process.argv.push('./test/test2.json');
+
+			settingsLib.initialize(
+				defaultOptions,
+				function (err, settings) {
+					should.not.exist(err);
+					should.exist(settings);
+					should.exist(settingsLib.commandLineConfig);
+
+					// clean up
+					process.argv = process.argv.slice(0, process.argv.length - 2);
+
+					done();
+				});
+		});
+
+		it('should choke loading command line config if file not found', function (done) {
+			process.argv.push('--config-file');
+			process.argv.push('./test/test2.not.there.json');
+
+			settingsLib.initialize(
+				defaultOptions,
+				function (err, settings) {
+					should.exist(err);
+					should.not.exist(settings);
+
+					// clean up
+					process.argv = process.argv.slice(0, process.argv.length - 2);
+
+					done();
+				});
+		});
+
+		it('should not allow extra fields to propegate from command line config if baseConfig is set', function (done) {
+			process.argv.push('--config-file');
+			process.argv.push('./test/test2.json');
+
+			settingsLib.initialize(
+				defaultOptions,
+				function (err, settings) {
+					should.not.exist(err);
+					should.exist(settings);
+					should.exist(settingsLib.commandLineConfig);
+					should.not.exist(settings["extra-key"]);
+
+					// clean up
+					process.argv = process.argv.slice(0, process.argv.length - 2);
+
+					done();
+				});
+		});
+
+		it('should override base config with command line config', function (done) {
+			process.argv.push('--config-file');
+			process.argv.push('./test/test2.json');
+
+			settingsLib.initialize(
+				defaultOptions,
+				function (err, settings) {
+					should.not.exist(err);
+					should.exist(settings);
+					should.exist(settingsLib.commandLineConfig);
+					settings["test-key"].should.equal('test-value-override-again');
+
+					// clean up
+					process.argv = process.argv.slice(0, process.argv.length - 2);
+
+					done();
+				});
+		});
+
+		it('should override both base and environment config with command line config', function (done) {
+			process.argv.push('--config-file');
+			process.argv.push('./test/test2.json');
+			process.env.NODE_ENV = 'test';
+			defaultOptions.environmentSearchPaths = ['./test'];
+
+			settingsLib.initialize(
+				defaultOptions,
+				function (err, settings) {
+					should.not.exist(err);
+					should.exist(settings);
+					should.exist(settingsLib.commandLineConfig);
+					should.exist(settingsLib.environmentConfig);
+					settings["test-key"].should.equal('test-value-override-again');
+
+					// clean up
+					process.argv = process.argv.slice(0, process.argv.length - 2);
+					delete process.env.NODE_ENV;
+
+					done();
+				});
+		});
+
+		it('should choke loading badly formatted command line config file', function (done) {
+			process.argv.push('--config-file');
+			process.argv.push('./test/bad.json');
+
+			settingsLib.initialize(
+				defaultOptions,
+				function (err, settings) {
+					should.exist(err);
+					should.not.exist(settings);
+
+					// clean up
+					process.argv = process.argv.slice(0, process.argv.length - 2);
+
+					done();
+				});
+		});
+
+		it('should do nothing if commandLineSwitches is not an array', function (done) {
+			process.argv.push('--config-file');
+			process.argv.push('./test/badly.formed.json');
+
+			defaultOptions.commandLineSwitches = 'not an array';
+
+			settingsLib.initialize(
+				defaultOptions,
+				function (err, settings) {
+					should.not.exist(err);
+					should.exist(settings);
+					should.not.exist(settingsLib.commandLineConfig);
+
+					// clean up
+					process.argv = process.argv.slice(0, process.argv.length - 2);
 
 					done();
 				});
